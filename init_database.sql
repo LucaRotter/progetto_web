6b69584372da17a51257cd5c423a0694345ad328
LucaRotter <lgrotter@studenti.uninsubria.it>
2025-05-16 14:55:20 +0200

    fixxato problemi di insert

--- init_database.sql
+++ init_database.sql
@@ -1,11 +1,11 @@
 create table roles(
 role_id varchar(5) primary key,
-name varchar(30) not null
+name varchar(100) not null
 );
 
 create table permissions(
 permission_id varchar(5) primary key,
-name varchar(30) not null
+name varchar(100) not null
 );
 
 create table roles_permissions(
@@ -16,23 +16,23 @@ primary key(permission_id, role_id)
 
 create table users(
 user_id varchar(5) primary key,
-name varchar(30) not null, 
-surname varchar(30) not null,
-email varchar(30) not null, 
-pwd varchar(30) not null,
+name varchar(100) not null, 
+surname varchar(100) not null,
+email varchar(100) not null, 
+pwd varchar(100) not null,
 image_url text,
 role_id varchar(5) references roles(role_id)
 );
 
 create table categories(
 category_id varchar(5) primary key,
-name varchar(30) not null,
+name varchar(100) not null,
 image_url text
 );
 
 create table items(
 item_id varchar(5) primary key,
-name varchar(30) not null,
+name varchar(100) not null,
 prezzo DECIMAL(10, 2) not null,
 description text,
 quantity int not null,
@@ -50,7 +50,7 @@ artisan_id varchar(5) references users(user_id),
 quantity int not null,
 day date not null,
 time TIME not null,
-state varchar(30) not null
+state varchar(100) not null
 );
 
 create table carts(
@@ -61,7 +61,7 @@ user_id varchar(5) references users(user_id)on delete cascade on update cascade
 
 create table reports(
 report_id varchar(5) primary key,
-category varchar(30) not null, 
+category varchar(100) not null, 
 description text,
 customer_id varchar(5) references users(user_id) on delete cascade on update cascade,
 artisan_id varchar(5) references users(user_id) on delete cascade on update cascade,
@@ -81,7 +81,7 @@ insert into roles(role_id, name)
 values(1, 'C'),(2, 'A'),(3, 'Ad');
 
 --permissions
-INSERT INTO permission(permission_id, name) VALUES
+INSERT INTO permissions(permission_id, name) VALUES
 --for the customer
 (1, 'update_cart'),
 (2, 'place_order'),
@@ -130,19 +130,20 @@ INSERT INTO roles_permissions(role_id, permission_id) VALUES
 
 --admin
 --DA MODIFICARE EMAIL E PWD
-INSERT INTO users(user_id, name, surname, email, pwd, image_url, role_id) VALUES
-(ad001, Alessandro, Grassi, alessandro.grassi24062003@gmail.com, admin1, https://res.cloudinary.com/dftu5zdbs/image/upload/v1746795022/https___images.genius.com_00fec6153b63522d6896275b0aedf9e3.1000x1000x1_snyln4.png, 3),
-(ad002, Aleksandar, Kastratovic, aleks.kastratovic@gmail.com, admin2, https://res.cloudinary.com/dftu5zdbs/image/upload/v1746794932/Screenshot_2025-05-09_144831_ufynui.png, 3),
-(ad003, Luca, Rotter, marketrader69@gmail.com, admin3, https://res.cloudinary.com/dftu5zdbs/image/upload/v1746723876/test_upload/dwurd5vegcxqy4xgwfqb.jpg, 3),
-(ad004, Davide, Bilora, davidebilora03pc@gmail.com, admin4, https://res.cloudinary.com/dftu5zdbs/image/upload/v1746794973/Screenshot_2025-05-09_144923_efkeg7.png, 3);
+INSERT INTO users(user_id, name, surname, email, pwd, image_url, role_id) VALUES 
+('ad001', 'Alessandro', 'Grassi', 'alessandro.grassi24062003@gmail.com', 'admin1', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1746795022/https___images.genius.com_00fec6153b63522d6896275b0aedf9e3.1000x1000x1_snyln4.png', 3),
+('ad002', 'Aleksandar', 'Kastratovic', 'aleks.kastratovic@gmail.com', 'admin2', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1746794932/Screenshot_2025-05-09_144831_ufynui.png', 3),
+('ad003', 'Luca', 'Rotter', 'marketrader69@gmail.com', 'admin3', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1746723876/test_upload/dwurd5vegcxqy4xgwfqb.jpg', 3),
+('ad004', 'Davide', 'Bilora', 'davidebilora03pc@gmail.com', 'admin4', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1746794973/Screenshot_2025-05-09_144923_efkeg7.png', 3);
 
 --CATEGORIE
 INSERT INTO categories(category_id, name, image_url) VALUES
-(cat01, libri, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
-(cat02, elettronica, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
-(cat03, abbigliamento, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
-(cat04, casa, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
-(cat05, giardino, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
-(cat06, informatica, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
-(cat07, sport, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
-(cat08, bellezza, https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg),
+('cat01', 'libri', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat02', 'elettronica', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat03', 'abbigliamento', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat04', 'casa', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat05', 'giardino', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat06', 'informatica', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat07', 'sport', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat08', 'bellezza', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg'),
+('cat09', 'cibo', 'https://res.cloudinary.com/dftu5zdbs/image/upload/v1747398272/OIP_tnnwhm.jpg');
