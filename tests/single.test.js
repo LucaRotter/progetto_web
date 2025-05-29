b6441af89dd9889d8648548249e7561b387f55e1
sooKyura <davidebilora03pc@gmail.com>
2025-05-29 17:52:12 +0200

    rimozione file caricati per errore

--- tests/single.test.js
+++
@@ -1,9 +0,0 @@
-//Singoli test di funzioni da aggiungere poi in server.test.js
-//AVVIARE QUESTO FILE CON IL COMANDO: npx jest single
-const request = require('supertest');
-const path = require('path');
-const fs = require('fs');
-const { app,pool, generateToken } = require('../server2'); // Assicurati che app e generateToken siano esportati
-
-
-
