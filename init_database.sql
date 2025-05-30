5eda6a29b237c9f8f11f6c9cbd565b1ac3a1fe73
LucaRotter <lgrotter@studenti.uninsubria.it>
2025-05-30 22:10:29 +0200

    sistemato index

--- init_database.sql
+++ init_database.sql
@@ -84,7 +84,7 @@ description text
 );
 
 create table shuffled(
-index varchar(5) primary key,
+item_index INTEGER primary key,
 item_id varchar(5) references items(item_id) on delete cascade on update cascade,
 user_key varchar(16) not null,
 category_id varchar(5) references categories(category_id) on delete cascade on update cascade
