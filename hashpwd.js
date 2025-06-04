be8271eb8506741f00f5cfcc65ad12896be07d79
LucaRotter <lgrotter@studenti.uninsubria.it>
2025-06-04 17:00:07 +0200

    prova docker con frontend temporaneo

--- hashpwd.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function run() {
-  const password = 'password123';
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log('Hashed Password:', hashedPassword);
-}
-
-run();
